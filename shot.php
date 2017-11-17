2f93d5bd131c99a8ea5efedc920af798125b1320
Phuong Huynh <phuonghuynh.net@gmail.com>
2017-11-17 11:30:02 +0700

    Fix run chrome on root

--- shot.php
+++ shot.php
@@ -6,6 +6,7 @@ $config = require './config.php';
 
 use dawood\phpChrome\Chrome;
 $chrome = new Chrome($config['template_url'], $config['chrome_path']);
+$chrome->setArgument('--no-sandbox', '');
 $chrome->setOutputDirectory(__DIR__.'/images');
 $chrome->setWindowSize(610, 800);
 $chrome->getScreenShot(__DIR__ .'/images/menu.jpg');
