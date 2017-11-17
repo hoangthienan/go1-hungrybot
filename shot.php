1a53a1531346cc2d118aa1e8b38999253c2a8e4d
Phuong Huynh <phuonghuynh.net@gmail.com>
2017-11-17 11:10:38 +0700

    Add shot

---
+++ shot.php
@@ -0,0 +1,12 @@
+<?php
+
+include './vendor/autoload.php';
+
+$config = require './config.php';
+
+use dawood\phpChrome\Chrome;
+$chrome = new Chrome($config['template_url'], $config['chrome_path']);
+$chrome->setOutputDirectory(__DIR__.'/images');
+$chrome->setWindowSize(610, 800);
+$chrome->getScreenShot(__DIR__ .'/images/menu.jpg');
+
