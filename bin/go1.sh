9d84df681961b2df119529270e459c1b4125aa74
An <hoangthienan@gmail.com>
2017-11-17 10:47:44 +0700

    Add supervisor

---
+++ bin/go1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+php=`which php`
+while true
+do
+    # Echo current date to stdout
+    echo 'Start :' `date`
+
+    $php /var/www/app/console go1:sheet >/dev/null 2>&1
+
+    # Echo 'error!' to stderr
+    echo 'Error! ' `date` >&2
+
+    sleep 60
+done
